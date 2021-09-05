e7c84edc9794af974b08491328b400ddf2c9fe6e
Tigran Saakyan <tigraqwerty@gmail.com>
2021-09-05 13:39:50 +0200

    ADD: Not found page implementation

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  styleUrls: ['./not-found.component.scss'],
+  host: {
+    class: 'router-container',
+  },
 })
-export class NotFoundComponent implements OnInit {
+export class NotFoundComponent {
+  constructor(private _router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public goBack(): void {
+    this._router.navigate(['/login']);
   }
-
 }
--- src/app/pages/not-found/not-found.module.ts
+++ src/app/pages/not-found/not-found.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonModule } from '@angular/material/button';
+
 import { NotFoundComponent } from './not-found.component';
 import { NotFoundRoutes } from './not-found.routing';
 
 @NgModule({
   declarations: [NotFoundComponent],
-  imports: [CommonModule, NotFoundRoutes],
+  imports: [CommonModule, MatButtonModule, NotFoundRoutes],
 })
 export class NotFoundModule {}
