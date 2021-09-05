df00adb88caefb4a9d4d683b92c1253bd4da2c30
Tigran Saakyan <tigraqwerty@gmail.com>
2021-09-05 14:25:32 +0200

    ADD: restfull provider implementation

--- src/app/core/services/request/providers/rest-api.provider.ts
+++ src/app/core/services/request/providers/rest-api.provider.ts
@@ -7,6 +7,7 @@ import { environment } from 'src/environments/environment';
 import { IAuth } from '@core/models/auth.interface';
 import { IRequestProvider } from '@core/models/request-provider.interface';
 import { IUser } from '@core/models/user.interface';
+import { CallModel } from '@core/models/call.model';
 
 export class RestAPIProvider implements IRequestProvider {
   constructor(private _httpClient: HttpClient) {}
@@ -27,14 +28,24 @@ export class RestAPIProvider implements IRequestProvider {
   }
 
   public getCalls(offset: number, limit: number): Observable<any> {
-    return new Observable();
+    let url = `${environment.apiURL.RestAPI}/calls?offset=${offset}&limit=${limit}`;
+    return this._httpClient
+      .get<{ nodes: CallModel[]; hasNextPage: boolean }>(url)
+      .pipe(map((res) => ({ calls: res.nodes, hasNextPage: res.hasNextPage })));
   }
 
   public archiveCall(id: string): Observable<{ id: string }> {
-    return new Observable();
+    let url = `${environment.apiURL.RestAPI}/calls/${id}/archive`;
+    return this._httpClient.put(url, {}).pipe(map((_) => ({ id })));
   }
 
   public getCallById(id: string): Observable<any> {
-    return new Observable();
+    let url = `${environment.apiURL.RestAPI}/calls/${id}`;
+    return this._httpClient.get<CallModel>(url).pipe(
+      map((res) => {
+        console.log(res);
+        return res;
+      })
+    );
   }
 }
